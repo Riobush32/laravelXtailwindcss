6a22897e4a7bb5485da7b6aab30536e53b7ea95f
Riobush32 <riobush32@gmail,com>
2022-06-07 11:08:02 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container w-full text-bold text-red-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Architecto quia unde tempore eos perspiciatis esse perferendis quidem, pariatur necessitatibus similique,
+        officiis autem. Earum optio, quidem, deserunt nisi deleniti vitae repudiandae ab impedit vel, quas error
+        voluptatibus recusandae. Ab cumque, perspiciatis beatae excepturi nulla aspernatur sunt?
+    </div>
+</body>
+
+</html>
